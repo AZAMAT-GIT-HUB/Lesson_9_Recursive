534efe6827b563b3bedf4a8c37edb82f86f281cd
Azamat Omirzak <u.azamat.1997@gmail.com>
2023-11-23 19:07:57 -0600

    Commit Homework

---
+++ Problem_64.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+void Recursive(int number_a){
+    if (number_a == 1) {
+        Console.Write(number_a + " ! ");
+      return;
+    }
+    Console.Write(number_a + ", ");
+    Recursive(number_a - 1);
+}
+
+Console.Write("Enter the number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Recursive(N);
---
+++ Problem_66.cs
@@ -0,0 +1,18 @@
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Recursive(int number_a, int number_b){
+    if(number_b == number_a){
+        
+        return number_a;
+    }
+    return number_b + Recursive(number_a, number_b-1);
+    }
+
+Console.Write("Enter the number_a: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number_b: ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+Console.Write("\nSum of number from N to N: " + Recursive(number_a, number_b));
